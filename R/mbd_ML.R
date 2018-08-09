dfa8db3c712e72d8202eb61d0ef16696b92cf910
Giappo <glaudanno@gmail.com>
2018-08-09 20:56:36 +0200

    mbd_ML can be silent

--- R/mbd_ML.R
+++ R/mbd_ML.R
@@ -41,19 +41,26 @@
 #' MBD:::mbd_ML(brts = simulated_data$brts, initparsopt = 0.11 ,idparsopt = 4,
 #' idparsfix = c(1,2,3), parsfix = test_pars[idparsfix], missnumspec = 0, cond = 1, soc = 2)
 #' @export
-mbd_ML <- function(brts, initparsopt, idparsopt, idparsfix = (1:4)[-idparsopt], parsfix,
-                   missnumspec = 0, cond = 1, soc = 2, tips_interval = c(0, Inf),
-                   res = 10 * (1 + length(brts) + missnumspec), tol = c(1E-3, 1E-4, 1E-6),
-                   maxiter = 1000 * round((1.25)^length(idparsopt)),
-                   changeloglikifnoconv = FALSE, optimmethod = 'simplex', methode = "expo",
-                   minimum_multiple_births = 0, pars.transform = 1, print_errors = 0, ...)
-  {# bracket#1
+mbd_ML <- function(
+  brts, initparsopt, idparsopt, idparsfix = (1:4)[-idparsopt], parsfix,
+  missnumspec = 0, cond = 1, soc = 2, tips_interval = c(0, Inf),
+  res = 10 * (1 + length(brts) + missnumspec), tol = c(1E-3, 1E-4, 1E-6),
+  maxiter = 1000 * round((1.25)^length(idparsopt)),
+  changeloglikifnoconv = FALSE, optimmethod = 'simplex', methode = "expo",
+  minimum_multiple_births = 0, pars.transform = 1, print_errors = 0, 
+  verbose = TRUE, 
+  ...
+)
+{# bracket#1
   # - tol = tolerance in optimization
   # - changeloglikifnoconv = if T the loglik will be set to -Inf if ML does not converge
   # - maxiter = the maximum number of iterations in the optimization
   # - changeloglikifnoconv = if T the loglik will be set to -Inf if ML does not converge
   # - optimmethod = 'simplex' (current default) or 'subplex' (default of previous versions)
-
+  if (!is.numeric(brts))
+  {
+    stop("'brts' must be numeric")
+  }
   if (length(idparsfix) == 0) {idparsfix <- NULL}
   if (missing(parsfix) && (length(idparsfix) == 0)){parsfix <- idparsfix <- NULL}
 
@@ -61,98 +68,111 @@ mbd_ML <- function(brts, initparsopt, idparsopt, idparsfix = (1:4)[-idparsopt], 
   namepars <- c("lambda", "mu", "nu", "q"); Npars <- length(namepars); #if you add more parameters to your model just change this
   failpars <- rep(-1, Npars); names(failpars) <- namepars; #those are the parameters that you get if something goes sideways
   if (is.numeric(brts) == FALSE)
-  {# bracket#2
+  {
     cat("The branching times should be numeric.\n")
     out2 <- data.frame(t(failpars), loglik = -1, df = -1, conv = -1)
+    return(invisible(out2))
+  }
+  idpars <- sort(c(idparsopt, idparsfix))
+  if ( (sum(idpars == (1:Npars)) != Npars) || (length(initparsopt) != length(idparsopt)) || (length(parsfix) != length(idparsfix)) )
+  {
+    cat("The parameters to be optimized and/or fixed are incoherent.\n")
+    out2 <- data.frame(t(failpars), loglik = -1, df = -1, conv = -1)
+    return(out2)
+  }
+  
+  if(length(namepars[idparsopt]) == 0) { optstr = "nothing" } else { optstr = namepars[idparsopt] }
+  if (verbose == TRUE) {
+    cat("You are optimizing",optstr,"\n")
+  }
+  if(length(namepars[idparsfix]) == 0) { fixstr = "nothing" } else { fixstr = namepars[idparsfix] }
+  if (verbose == TRUE) {
+    cat("You are fixing",fixstr,"\n")
+    cat("Optimizing the likelihood - this may take a while.","\n")
+    flush.console()
+  }
+  if (pars.transform == 1)
+  {
+    #Rampal's transformation
+    trparsopt = initparsopt/(1 + initparsopt)
+    trparsopt[which(initparsopt == Inf)] = 1
+    trparsfix = parsfix/(1 + parsfix)
+    trparsfix[which(parsfix == Inf)] = 1
   }else
-  {# bracket#2
-    idpars <- sort(c(idparsopt, idparsfix))
-    if ( (sum(idpars == (1:Npars)) != Npars) || (length(initparsopt) != length(idparsopt)) || (length(parsfix) != length(idparsfix)) )
-    {# bracket#3
-      cat("The parameters to be optimized and/or fixed are incoherent.\n")
-      out2 <- data.frame(t(failpars), loglik = -1, df = -1, conv = -1)
-    }else
-    {# bracket#3
-      if(length(namepars[idparsopt]) == 0) { optstr = "nothing" } else { optstr = namepars[idparsopt] }
-      cat("You are optimizing",optstr,"\n")
-      if(length(namepars[idparsfix]) == 0) { fixstr = "nothing" } else { fixstr = namepars[idparsfix] }
-      cat("You are fixing",fixstr,"\n")
-      cat("Optimizing the likelihood - this may take a while.","\n")
-      flush.console()
-      if (pars.transform == 1)
-      {
-        #Rampal's transformation
-        trparsopt = initparsopt/(1 + initparsopt)
-        trparsopt[which(initparsopt == Inf)] = 1
-        trparsfix = parsfix/(1 + parsfix)
-        trparsfix[which(parsfix == Inf)] = 1
-      }else
-      {
-        trparsopt  <- initparsopt
-        trparsfix  <- parsfix
-      }
-      optimpars  <- c(tol, maxiter)
-      initloglik <- MBD:::mbd_loglik_choosepar(trparsopt = trparsopt, trparsfix = trparsfix,
-                                               idparsopt = idparsopt, idparsfix = idparsfix,
-                                               brts = brts, missnumspec = missnumspec,
-                                               cond = cond, soc = soc, tips_interval = tips_interval,
-                                               methode = methode,
-                                               minimum_multiple_births = minimum_multiple_births,
-                                               pars.transform = pars.transform,
-                                               print_errors = print_errors, ...) #there's no pars2 here and instead 3 more args at the end
-      cat("The loglikelihood for the initial parameter values is",initloglik,"\n")
-      flush.console()
-      if (initloglik == -Inf)
-      {# bracket#4
-        cat("The initial parameter values have a likelihood that is equal to 0 or below machine precision. Try again with different initial values.\n")
-        out2 <- data.frame(t(failpars), loglik = -1, df = -1, conv = -1)
-      }else
-      {# bracket#4
-        out <- DDD:::optimizer(optimmethod = optimmethod, optimpars = optimpars,
-                               fun = MBD:::mbd_loglik_choosepar,
-                               trparsopt = trparsopt, trparsfix = trparsfix,
-                               idparsopt = idparsopt, idparsfix = idparsfix,
-                               brts = brts, missnumspec = missnumspec, cond = cond,
-                               soc = soc, tips_interval = tips_interval, methode = methode,
-                               minimum_multiple_births = minimum_multiple_births,
-                               pars.transform = pars.transform, print_errors = print_errors, ...)
-        if (out$conv != 0)
-        {# bracket#5
-          cat("Optimization has not converged. Try again with different initial values.\n")
-          out2 <- data.frame(t(failpars), loglik = -1, df = -1, conv = -1)
-        }else
-        {# bracket#5
-          MLtrpars <- as.numeric(unlist(out$par))
-          if (pars.transform == 1)
-          {
-            #Rampal's transformation
-            MLpars = MLtrpars/(1 - MLtrpars)
-          }else
-          {
-            MLpars <- MLtrpars
-          }
-          MLpars1 <- rep(0, Npars); names(MLpars1) <- namepars
-          MLpars1[idparsopt] <- MLpars
-          if (length(idparsfix) != 0) {MLpars1[idparsfix] <- parsfix}
-          ML <- as.numeric(unlist(out$fvalues))
-          out2 <- data.frame(t(MLpars1), loglik = ML, df = length(initparsopt), conv = unlist(out$conv))
+  {
+    trparsopt  <- initparsopt
+    trparsfix  <- parsfix
+  }
+  optimpars  <- c(tol, maxiter)
+  initloglik <- MBD:::mbd_loglik_choosepar(trparsopt = trparsopt, trparsfix = trparsfix,
+                                           idparsopt = idparsopt, idparsfix = idparsfix,
+                                           brts = brts, missnumspec = missnumspec,
+                                           cond = cond, soc = soc, tips_interval = tips_interval,
+                                           methode = methode,
+                                           minimum_multiple_births = minimum_multiple_births,
+                                           pars.transform = pars.transform,
+                                           print_errors = print_errors, ...) #there's no pars2 here and instead 3 more args at the end
+  if (verbose == TRUE) {
+    cat("The loglikelihood for the initial parameter values is",initloglik,"\n")
+    flush.console()
+  }
+  if (initloglik == -Inf)
+  {# bracket#4
+    cat("The initial parameter values have a likelihood that is equal to 0 or below machine precision. Try again with different initial values.\n")
+    out2 <- data.frame(t(failpars), loglik = -1, df = -1, conv = -1)
+    return (invisible(out2))
+  }
+  if (verbose == TRUE) {
+    sink(file = tempfile()) # Sink output here
+  }
+  out <- DDD:::optimizer(optimmethod = optimmethod, optimpars = optimpars,
+                         fun = MBD:::mbd_loglik_choosepar,
+                         trparsopt = trparsopt, trparsfix = trparsfix,
+                         idparsopt = idparsopt, idparsfix = idparsfix,
+                         brts = brts, missnumspec = missnumspec, cond = cond,
+                         soc = soc, tips_interval = tips_interval, methode = methode,
+                         minimum_multiple_births = minimum_multiple_births,
+                         pars.transform = pars.transform, print_errors = print_errors, ...)
+  if (verbose == TRUE) {
+    sink() # Give back the output
+  }
+  if (out$conv != 0)
+  {# bracket#5
+    cat("Optimization has not converged. Try again with different initial values.\n")
+    out2 <- data.frame(t(failpars), loglik = -1, df = -1, conv = -1)
+    return(invisible(out2))
+  }
+  MLtrpars <- as.numeric(unlist(out$par))
+  if (pars.transform == 1)
+  {
+    #Rampal's transformation
+    MLpars = MLtrpars/(1 - MLtrpars)
+  }else
+  {
+    MLpars <- MLtrpars
+  }
+  MLpars1 <- rep(0, Npars); names(MLpars1) <- namepars
+  MLpars1[idparsopt] <- MLpars
+  if (length(idparsfix) != 0) {MLpars1[idparsfix] <- parsfix}
+  ML <- as.numeric(unlist(out$fvalues))
+  out2 <- data.frame(t(MLpars1), loglik = ML, df = length(initparsopt), conv = unlist(out$conv))
 
-          tobeprint <- "Maximum likelihood parameter estimates:"
-          for (ii in 1:Npars)
-          {
-            tobeprint <- paste(tobeprint,paste(names(MLpars1[ii]),":",sep = ""),MLpars1[ii])
-          }
-          s1 <- sprintf(tobeprint)
-
-          if(out2$conv != 0 & changeloglikifnoconv == T) {out2$loglik <- -Inf}
-          s2 <- sprintf('Maximum loglikelihood: %f',ML)
-          cat("\n",s1,"\n",s2,"\n\n")
-        }# bracket#5
-      }# bracket#4
-    }# bracket#3
-  }# bracket#2
+  tobeprint <- "Maximum likelihood parameter estimates:"
+  for (ii in 1:Npars)
+  {
+    tobeprint <- paste(tobeprint,paste(names(MLpars1[ii]),":",sep = ""),MLpars1[ii])
+  }
+  if (verbose == TRUE) {
+    s1 <- sprintf(tobeprint)
+  }
+  if(out2$conv != 0 & changeloglikifnoconv == T) {out2$loglik <- -Inf}
+  if (verbose == TRUE) {
+    s2 <- sprintf('Maximum loglikelihood: %f',ML)
+    cat("\n",s1,"\n",s2,"\n\n")
+  }
+  
   invisible(out2)
-}# bracket#1
+}
 
 # mbd_ML_cluster----------------
 #' @author Giovanni Laudanno
---
+++ tests/testthat/test-mbd_ML.R
@@ -0,0 +1,36 @@
+context("mbd_ML")
+
+test_that("mbd_ML can be silent", {
+
+  set.seed(10)
+  test_pars <- c(0.3, 0.1, 0.1, 0.15)
+  idparsfix <- c(1,2,3)
+  
+  testthat::expect_silent(
+    MBD:::mbd_ML(
+      brts = c(1,2,3), 
+      initparsopt = 0.11,
+      idparsopt = 4,
+      idparsfix = idparsfix, 
+      parsfix = test_pars[idparsfix], 
+      missnumspec = 0, 
+      cond = 1, 
+      soc = 2,
+      verbose = FALSE
+    )
+  )
+
+})
+
+test_that("abuse", {
+  
+  testthat::expect_error(
+    MBD:::mbd_ML(
+      brts = "nonsense", 
+      initparsopt = 0.11,
+      idparsopt = 4,
+      idparsfix = c(1,2,3), parsfix = test_pars[idparsfix], missnumspec = 0, cond = 1, soc = 2
+    ),
+    "'brts' must be numeric"
+  )
+})
