3af0b3c59f5a8f21ee8497442e09ea85713108e6
richelbilderbeek <richel@richelbilderbeek.nl>
2018-09-26 11:29:49 +0200

    Add 'create_mbd_params'

---
+++ tests/testthat/test-mbd_ml2.R
@@ -0,0 +1,44 @@
+context("mbd_ml2")
+
+test_that("use", {
+
+  skip("WIP")
+  set.seed(10)
+  lambda <- 0.3
+  mu <- 0.1
+  nu <- 0.11
+  q < 0.15
+  test_pars <- c(lambda, mu, nu, q)
+  idparsfix <- c(1, 2, 3)
+  out_classic <- mbd_ml(
+    brts = c(1, 2, 3),
+    initparsopt = 0.11,
+    idparsopt = 4,
+    idparsfix = idparsfix,
+    parsfix = test_pars[idparsfix],
+    missnumspec = 0,
+    cond = 1,
+    soc = 2,
+    verbose = FALSE
+  )
+  expect_equal(lambda, out_classic$lambda)
+  expect_equal(mu, out_classic$mu)
+  expect_equal(nu, out_classic$nu)
+  expect_equal(0.0010006043479098515256, out_classic$q)
+  
+  out <- mbd_ml2(
+    branching_times = c(1, 2, 3),
+    init_param_values = create_mbd_params(
+      lambda = lambda,
+      mu = mu,
+      nu = nu,
+      q = q
+    ),
+    fixed_params = c("lambda", "mu", "nu"),
+    estimated_params = c("q"),
+    init_n_species = 2,
+    n_missing_species = 0,
+    conditioned_on = "non_extinction"
+  )
+})
+  
