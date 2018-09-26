4ae79695df0245b94d20514d40e7c378a5f363c5
richelbilderbeek <richel@richelbilderbeek.nl>
2018-09-26 12:49:44 +0200

    Fix doc

--- R/is_mbd_params.R
+++ R/is_mbd_params.R
@@ -1,5 +1,7 @@
 #' Is object \code{x} an MBD parameter set, 
 #'   as can be created by \code{create_mbd_params}?
+#' @param x the object to be determined of if
+#'   its an MBD parameter set
 #' @return TRUE if yes, else FALSE 
 #' @author Richel J.C. Bilderbeek
 is_mbd_params <- function(x) {
