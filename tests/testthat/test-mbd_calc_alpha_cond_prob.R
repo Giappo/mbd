ae7b362931b68c47ed09c01982ccd56a88745d76
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-02 12:42:39 +0200

    Renamed 'alpha_conditional_probability' to 'mbd_calc_alpha_cond_prob' and put it in 'mbd_calc_alpha_cond_prob'

--- tests/testthat/test-mbd_calc_alpha_cond_prob.R
+++ tests/testthat/test-mbd_calc_alpha_cond_prob.R
@@ -3,7 +3,7 @@ context("alpha_conditional_probability")
 test_that("abuse", {
   
   expect_error(
-    alpha_conditional_probability(
+    mbd_calc_alpha_cond_prob(
       brts = c(1, 2, 3), 
       pars = c(0.1, 0.2, 0.3, 0.4), 
       alpha = 10^20 # Big
