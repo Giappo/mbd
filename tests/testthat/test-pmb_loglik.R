f45b899f98657839f8bc34cb689f9a98cada9269
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-04 12:29:41 +0200

    Properly test pmb_loglik

--- tests/testthat/test-pmb_loglik.R
+++ tests/testthat/test-pmb_loglik.R
@@ -2,27 +2,11 @@ context("pmb_loglik")
 
 test_that("use", {
 
-  expect_silent(
-    mbd::pmb_loglik(
-      pars = c(0.2, 0.15, 2.0, 0.1),
-      brts = c(1, 2, 3),
-      soc = 2 # Crown age
-    )
-  )
-})
-
-test_that("Convergence fail", {
-  lambda <- 0.1
-  mu <- 0.2
-  nu <- 0.3
-  q <- 0.4
-  pars <- c(lambda, mu, nu, q)
-  crown <- 2
-  # Here it works
+  # pars[2] must be zero
   loglik <- mbd::pmb_loglik(
-    pars = pars,
-    brts = c(1, 2, 2),
-    soc = crown
+    pars = c(0.2, 0.0, 2.0, 0.1),
+    brts = c(1, 2, 3),
+    soc = 2 # Crown age
   )
-  expect_equal(loglik, -Inf)
+  expect_true(loglik > -Inf)
 })
