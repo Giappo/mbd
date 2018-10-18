35fbadf70f7e85018954d4ee427edb1923bec438
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-18 10:23:43 +0200

    Add https://github.com/richelbilderbeek/razzo/issues/24

---
+++ tests/testthat/test-mbd_count_n_spec_events.R
@@ -0,0 +1,21 @@
+context("mbd_count_n_spec_events")
+
+test_that("no event", {
+  
+  skip("https://github.com/richelbilderbeek/razzo/issues/24")
+  newick <- "((A:1, B:1):1);"
+  tree <- ape::read.tree(text = newick)
+  ape::plot.phylo(tree)
+  n_events <- mbd_count_n_spec_events(tree)
+  expect_equal(1, n_events)
+})
+
+test_that("one event", {
+  
+  skip("https://github.com/richelbilderbeek/razzo/issues/24")
+  newick <- "((A:1, B:1):1, (C:1, D:1):1);"
+  tree <- ape::read.tree(text = newick)
+  ape::plot.phylo(tree)
+  n_events <- mbd_count_n_spec_events(tree)
+  expect_equal(1, n_events)
+})
