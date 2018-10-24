b779ebf1d6e69f1fc9e685439aa24fb9e8d08a31
richelbilderbeek <richel@richelbilderbeek.nl>
2018-10-24 16:33:18 +0200

    'mbd_sim' checks 'soc' argument

--- tests/testthat/test-mbd_sim.R
+++ tests/testthat/test-mbd_sim.R
@@ -41,4 +41,18 @@ test_that("abuse", {
     ),
     "'tips_interval' must contain two positive values"
   )
+  expect_error(
+    mbd_sim(
+      pars = c(1.0, 1.0, 1.0, 1.0),
+      soc = "nonsense"
+    ),
+    "'soc' must be numeric"
+  )
+  expect_error(
+    mbd_sim(
+      pars = c(1.0, 1.0, 1.0, 1.0),
+      soc = 42
+    ),
+    "'soc' must be '1' or '2'"
+  )
 })
