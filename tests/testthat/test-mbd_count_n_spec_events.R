d707af0ffb6e2d92f4f1431b71d597a918ffab3a
Giappo <glaudanno@gmail.com>
2018-11-07 17:39:26 +0100

    tested mbd_count_n_spec_events

--- tests/testthat/test-mbd_count_n_spec_events.R
+++ tests/testthat/test-mbd_count_n_spec_events.R
@@ -2,20 +2,18 @@ context("mbd_count_n_spec_events")
 
 test_that("no event", {
 
-  skip("https://github.com/richelbilderbeek/razzo/issues/24")
   newick <- "((A:1, B:1):1);"
   tree <- ape::read.tree(text = newick)
   ape::plot.phylo(tree)
-  n_events <- mbd_count_n_spec_events(tree)
-  expect_equal(1, n_events)
+  n_events <- mbd_count_n_spec_events(ape::branching.times(tree))
+  expect_equal(0, n_events)
 })
 
 test_that("one event", {
 
-  skip("https://github.com/richelbilderbeek/razzo/issues/24")
   newick <- "((A:1, B:1):1, (C:1, D:1):1);"
   tree <- ape::read.tree(text = newick)
   ape::plot.phylo(tree)
-  n_events <- mbd_count_n_spec_events(tree)
+  n_events <- mbd_count_n_spec_events(ape::branching.times(tree))
   expect_equal(1, n_events)
 })
