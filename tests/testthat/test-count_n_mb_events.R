daa9c9cd9ce65c63c6bfff3b1c90f9b4d50af624
Giappo <glaudanno@gmail.com>
2019-09-10 20:03:18 +0200

    added count_n_mb_events

--- tests/testthat/test-count_n_mb_events.R
+++ tests/testthat/test-count_n_mb_events.R
@@ -1,10 +1,10 @@
-context("mbd_count_n_spec_events")
+context("count_n_mb_events")
 
 test_that("no event", {
 
   newick <- "((A:1, B:1):1);"
   tree <- ape::read.tree(text = newick)
-  n_events <- mbd_count_n_spec_events(ape::branching.times(tree))
+  n_events <- count_n_mb_events(ape::branching.times(tree))
   expect_equal(0, n_events)
 })
 
@@ -12,6 +12,6 @@ test_that("one event", {
 
   newick <- "((A:1, B:1):1, (C:1, D:1):1);"
   tree <- ape::read.tree(text = newick)
-  n_events <- mbd_count_n_spec_events(ape::branching.times(tree))
+  n_events <- count_n_mb_events(ape::branching.times(tree))
   expect_equal(1, n_events)
 })
