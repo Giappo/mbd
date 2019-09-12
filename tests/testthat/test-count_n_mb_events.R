e48a3ea01cc79cc7e8025107dbd89d810005d14b
Giappo <glaudanno@gmail.com>
2019-09-12 20:02:27 +0200

    changed test-count_n_mb_events

--- tests/testthat/test-count_n_mb_events.R
+++ tests/testthat/test-count_n_mb_events.R
@@ -8,10 +8,10 @@ test_that("no event", {
   expect_equal(0, n_events)
 })
 
-test_that("one event", {
+test_that("two event", {
 
   newick <- "((A:1, B:1):1, (C:1, D:1):1);"
   tree <- ape::read.tree(text = newick)
   n_events <- count_n_mb_events(ape::branching.times(tree))
-  expect_equal(1, n_events)
+  expect_equal(2, n_events)
 })
