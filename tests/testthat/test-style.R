6d2633187bb9b7f29e6c12c39ea16dd538ff40fb
G. Laudanno <P274829@workspace.rug.nl>
2019-10-25 17:39:49 +0200

    updated lintr does not need cyclomatic check

--- tests/testthat/test-style.R
+++ tests/testthat/test-style.R
@@ -6,26 +6,6 @@ is_on_ci <- function() {
   is_it_on_appveyor || is_it_on_travis # nolint internal function
 }
 
-test_that("cyclomatic complexity", {
-
-  if (is_on_ci()) {
-    skip("Already performed by lintrbot.")
-  }
-
-  max_complexity <- 15
-  out <- cyclocomp::cyclocomp_package("mbd")
-  too_complex <- !all(out$cyclocomp < max_complexity)
-  if (too_complex) {
-    print(
-      "These functions are too complex:"
-    )
-    print(
-      out$name[out$cyclocomp >= max_complexity]
-    )
-  }
-  expect_false(too_complex)
-})
-
 test_that("package style", {
   lintr::expect_lint_free()
 })
