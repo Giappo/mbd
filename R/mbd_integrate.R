4ac5476a53a7ae886a5e4c5505582bbd023e452f
rsetienne <rampaletienne@gmail.com>
2019-10-26 17:37:40 +0200

    First commit of FORTRAN-related code. Nothing tested.

---
+++ R/mbd_integrate.R
@@ -0,0 +1,62 @@
+#' @useDynLib DDD
+mbd_ode_FORTRAN <- function(
+  initprobs,
+  tvec,
+  parsvec,
+  atol,
+  rtol,
+  methode,
+  runmod = "mbd_runmod"
+)
+{
+  N <- length(initprobs)
+  probs <- deSolve::ode(y = initprobs, parms = c(N + 0.), rpar = parsvec, 
+                        times = tvec, func = runmod, initfunc = "mbd_initmod", 
+                        ynames = c("SV"), dimens = N + 1, nout = 1, outnames = c("Sum"), 
+                        dllname = "mbd",atol = atol, rtol = rtol, method = methode)
+  #[,1:(N + 1)]
+  #if this fails then N + 2 instead of N + 1, or N?
+  return(probs)
+}
+
+mbd_integrate <- function(
+  y = y,
+  times = tseq,
+  func = func,
+  parms = parms,
+  atol = atol,
+  rtol = rtol,
+  tcrit = t1
+)
+{
+  func_name <- 'no_name'
+  if(is.character(func))
+  {
+    func_name <- func
+    }  
+  if(func_name == 'no_name')
+  {
+    out <- deSolve::ode(
+      y = y,
+      times = tseq,
+      func = func,
+      parms = parms,
+      atol = atol,
+      rtol = rtol,
+      tcrit = t1
+    )
+  } else
+  {
+    #func_name <- 'mbd_runmod'
+    out <- mbd_ode_FORTRAN(
+      initprobs = y,
+      tvec = tseq,
+      parsvec = parms,
+      atol = atol,
+      rtol = rtol,
+      methode = 'ode45',
+      runmod = func_name
+    )
+  }
+  return(out)
+}
---
+++ src/mbd_loglik_rhs_FORTRAN.f95
@@ -0,0 +1,116 @@
+! Helper function: 
+! fill vec with N elements from parms, starting at position ii
+!==========================================================================
+
+      SUBROUTINE mbd_fill1d (vec, DIMP, parms, II)
+      IMPLICIT NONE
+      INTEGER DIMP, II, I
+      DOUBLE PRECISION vec(DIMP), parms(*)
+      II = II
+        DO I = 1, DIMP
+          II = II + 1
+          vec(I) = parms(II)
+        ENDDO
+        
+      END SUBROUTINE mbd_fill1d
+
+!==========================================================================
+! module with declarations
+!==========================================================================
+
+      MODULE mbd_dimmod
+
+      ! length of the vector -  decided in R-code
+      INTEGER  :: N
+
+      ! 1 parameter vectors with unknown length
+      DOUBLE PRECISION, ALLOCATABLE  :: P(:)  
+      
+      ! Boolean: will become TRUE if the parameters have a value
+      LOGICAL :: initialised = .FALSE.
+
+      END MODULE mbd_dimmod
+
+!==========================================================================
+!==========================================================================
+! Initialisation: name of this function as passed by "initfunc" argument
+! Sets the fixed parameter vector, and allocates memory
+!==========================================================================
+!==========================================================================
+
+      SUBROUTINE mbd_initmod (steadyparms)
+      USE mbd_dimmod 
+
+      IMPLICIT NONE
+      EXTERNAL steadyparms
+
+      INTEGER, PARAMETER :: nparsmall = 1  ! constant-length parameters
+      
+      DOUBLE PRECISION parms(nparsmall)
+      COMMON /XCBPar/parms                 ! common block 
+
+! Set the fixed parameters obtained from R
+      CALL steadyparms(nparsmall, parms)
+
+! first parameter has the length of the vector       
+      N = INT(parms(1) + 1e-6)
+
+! Allocate variable size arrays (state variables, derivatives and parameters)
+
+      IF (ALLOCATED(P)) DEALLOCATE(P)  
+      ALLOCATE(P(N ** 2))
+
+      initialised = .FALSE.
+       
+      END SUBROUTINE mbd_initmod
+      
+!==========================================================================
+!==========================================================================
+! Dynamic routine: name of this function as passed by "func" argument
+! variable parameter values are passed via yout
+!==========================================================================
+!==========================================================================
+       
+      SUBROUTINE mbd_runmod (neq, t, Conc, dConc, yout, ip)
+      USE mbd_dimmod
+      IMPLICIT NONE
+
+!......................... declaration section.............................
+      INTEGER           :: neq, ip(*), i, ii
+      DOUBLE PRECISION  :: t, Conc(N), yout(*)
+      REAL(16)          :: dConc(N)
+
+! parameters - named here
+      DOUBLE PRECISION rn(1)
+      COMMON /XCBPar/rn
+
+! local variables
+      CHARACTER(len=100) msg
+
+!............................ statements ..................................
+
+      IF (.NOT. Initialised) THEN
+        ! check memory allocated to output variables
+        IF (ip(1) < 1) CALL rexit("nout not large enough") 
+
+        ! save parameter values in yout
+        ii = ip(1)   ! Start of parameter values
+        CALL mbd_fill1d(P, N ** 2, yout, ii)   ! ii is updated in fill1d
+        Initialised = .TRUE.          ! to prevent from initialising more than once
+      ENDIF
+
+! dynamics
+
+!mbd_loglik_rhs <- function(t, x, params) {
+!  list(params %*% x)
+!}
+
+      DO I = 1, N
+        dConc(I) = 0
+        DO J = 1, N
+          dConc(I) = dConc(I) + P((I - 1) * N + J) * Conc(J)
+        ENDDO
+      ENDDO
+  
+      END SUBROUTINE mbd_runmod
+      
